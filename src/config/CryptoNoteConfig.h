af18aed53ae496dbbb550578ff0bc5a123d67737
Roy Hodge <roy.hodge@mail.com>
2020-02-02 23:11:57 -0600

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -226,8 +226,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const uint64_t BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  11899;
-const int      RPC_DEFAULT_PORT                              =  11888;
+const int      P2P_DEFAULT_PORT                              =  1111;
+const int      RPC_DEFAULT_PORT                              =  2020;
 const int      SERVICE_DEFAULT_PORT                          =  8070;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
